8cd3d6b8955b97619b6a0b9cb7f369c80308afbb
Alvi Dervishaj <alvidervishaj9@gmail.com>
2023-08-04 13:38:16 +0200

    Updated to match the new websocket implementation.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,49 @@
 'use client';
 // React
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 // Material UI
 import { Grid, Button, Modal, Typography, Box } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 // Utils
-import { ApiDevices, DevicesList } from "@/utils";
-import { Process } from "@/Process";
+import { ApiDevices, DevicesList, Device, EmitTypes, EventTypes } from "@/utils";
 // Hooks
 import { useInterval } from "usehooks-ts";
+import { io, Socket } from "socket.io-client";
 
 function Phones() {
-  const [devices, setDevices] = useState<ApiDevices>([]);
+  const socket: Socket = io("ws://localhost:3030/");
+  const [devices, setDevices] = useState<Device[]>([]);
   const [isOpen, setIsOpen] = useState<boolean>(false);
-  const [selectedDevice, setSelectedDevice] = useState<{ id: string, name: string, process: Process | null, battery: string }>({ id: "", name: "", battery: "", process: null })
+  const [selectedDevice, setSelectedDevice] = useState<{ id: string, name: string, process: { username: string, configFile: string } | null, battery: string }>({ id: "", name: "", battery: "", process: null })
   const lightTheme = createTheme({ palette: { mode: "light" } });
   const handleClose = () => {
     setIsOpen(false);
     return;
   }
 
+  socket.on<EmitTypes>("get-devices-message", (devices: { _id: string, _name: string, _battery: string, _process: null | { username: string, configFile: string } }[]) => {
+    if (devices.length > 0) {
+      let temp: Device[] = [];
+      devices.forEach((_device) => {
+        temp.push(new Device(_device._id, _device._name, _device._battery, _device._process));
+      })
+      setDevices([...temp]);
+      return;
+    }
+    setDevices([]);
+    return;
+  })
+
+  useEffect(() => {
+    return () => {
+      socket.disconnect()
+    };
+  }, [socket])
+
 
   const fetchDevices = async () => {
-    const result = await fetch('/api/fetchDevices');
-    const data = await result.json();
-    setDevices(data);
+    socket.emit<EventTypes>("get-devices");
   }
 
   const handleSelection = (id: string) => {
@@ -38,7 +56,7 @@ function Phones() {
       setSelectedDevice({ id: '', name: "Device not found !", process: null, battery: "X" });
     }
   }
-  useInterval(fetchDevices, 1000 * 10);
+  useInterval(fetchDevices, 1000 * 15);
 
   const modalStyling = {
     position: 'absolute' as 'absolute',
@@ -54,21 +72,21 @@ function Phones() {
 
   return (
     <ThemeProvider theme={lightTheme}>
-    <Typography variant="h2" align={"center"}>Devices</Typography>
+      <Typography variant="h2" align={"center"}>Devices</Typography>
       <Box sx={{ flexGrow: 1, padding: '2rem 3rem' }}>
         <Grid container spacing={4}>
           {
             Object.entries(DevicesList).map(([key, value]: [key: string, value: string]) => {
               if (devices.find(element => element.id === key && element.name === value)) {
                 return <Grid item key={key} lg={1}>
-                  <Button sx={{padding: 1}} onClick={(event) => handleSelection(event.currentTarget.value)} variant="contained" value={key} color="info" key={`${value} ${key}`}>
+                  <Button sx={{ padding: 1 }} onClick={(event) => handleSelection(event.currentTarget.value)} variant="contained" value={key} color="info" key={`${value} ${key}`}>
                     {value}
                   </Button>
                 </Grid>
               }
               else {
                 return <Grid item key={key} lg={1} >
-                  <Button sx={{padding: 1}} onClick={(event) => handleSelection(event.currentTarget.value)} value={key} variant="contained" color="error" key={`${key} ${value}`}>
+                  <Button sx={{ padding: 1 }} onClick={(event) => handleSelection(event.currentTarget.value)} value={key} variant="contained" color="error" key={`${key} ${value}`}>
                     {value}
                   </Button>
                 </Grid>
--- src/utils.ts
+++ src/utils.ts
@@ -1,57 +1,111 @@
 import { Process } from "./Process";
 
 
+// Event Server Types
+export type EventTypes = "get-processes" |
+  "get-config" |
+  "get-session" |
+  "create-devices" |
+  "terminate-process" |
+  "send-status-to-telegram" |
+  "preview-device" |
+  "get-process" |
+  "create-process" |
+  "update-process" |
+  "update-processes" |
+  "stop-process" |
+  "create-device" |
+  "get-device" |
+  "update-device" |
+  "get-devices" |
+  "remove-device" |
+  "delete-older-logs" |
+  "read-config" |
+  "get-pid" |
+  "start-bot" |
+  "start-bot-checks" |
+  "remove-process";
+export type EmitTypes = `${EventTypes}-message`;
+
+export class Device {
+  private _id: string;
+  private _name: string;
+  private _battery: string;
+  private _process: { username: string, configFile: string } | null;
+
+  constructor(id: string, name: string, battery: string, process: { username: string, configFile: string } | null) {
+    this._id = id;
+    this._name = name;
+    this._battery = battery;
+    this._process = process;
+  }
+
+  get id(): string {
+    return this._id;
+  }
+  get battery(): string {
+    return this._battery;
+  }
+
+  get process(): { username: string, configFile: string } | null {
+    return this._process;
+  }
+  get name(): string {
+    return this._name;
+  }
+}
+
 export const DevicesList = {
-    "R9ZW305K08Z": "A01",
-    "R7AT310QB5L": "A02",
-    "R7ST405PFVZ": "A03",
-    "R7AT310PKFE": "A04",
-    "R7AT310N6YN": "A05",
-    "R7AT310PDHN": "A06",
-    "R7ST405MD7M": "A07",
-    "R7AT310PE5A": "A08",
-    "R7ST31LDCJH": "B09",
-    "R7AT310PDQK": "A10",
-    "R7AT310MW0F": "A11",
-    "R7AT310Q61D": "A12",
-    "R7ST30V0XHR": "A13",
-    "R7ST31LD00J": "B14",
-    "R7AT310HMKH": "A15",
-    "R7ST405K51K": "A16",
-    "R7AT310P7CM": "A17",
-    "R9YT805BYNL": "A18",
-    "R7AT310P7AV": "A19",
-    "R7ST405MTZP": "A20",
-    "R7SRC1WTKFA": "B21",
-    "R7ST30V31RK": "A22",
-    "R7AT310HPTF": "A23",
-    "R7ST405PJGR": "A24",
-    "R7ST30TVVJE": "C25",
-    "R7AT310NYTR": "A30",
-    "R7ST30TZZ0J": "A31",
-    "R7AT310PLGR": "A32",
-    "R7AT310Q98N": "A33",
-    "R7ST12LK1QJ": "B34",
-    "R7AT310MPTY": "A35",
-    "R7AT310Q6PY": "A36",
-    "R7ST30TZN5V": "A37",
-    "R7AT310PHJB": "A38",
-    "R7AT310NHRE": "A39",
-    "R7ST30V5MHT": "A40",
-    "R7ST405M05E": "A41",
-    "R7AT310P8AB": "A42",
-    "R7AT310QMVP": "A43",
-    "R7ST405K7PP": "A44",
-    "R7AT41BHF2H": "A45",
-    "R7ST30TYCHE": "A46",
-    "R7AT310J26E": "A47",
-    "R7AT310FTBJ": "A48",
-    "R7STB0PS5AA": "A49",
-    "R7AT310NZMK": "A50",
-    "R7ST12LG1QF": "A51",
-    "R7ST31L8DWN": "A52",
-    "R7AT30TZY2R": "A53",
-    "ca2937e9": "A54",
+  "R9ZW305K08Z": "A01",
+  "R7AT310QB5L": "A02",
+  "R7ST405PFVZ": "A03",
+  "R7AT310PKFE": "A04",
+  "R7AT310N6YN": "A05",
+  "R7AT310PDHN": "A06",
+  "R7ST405MD7M": "A07",
+  "R7AT310PE5A": "A08",
+  "R7ST31LDCJH": "B09",
+  "R7AT310PDQK": "A10",
+  "R7AT310MW0F": "A11",
+  "R7AT310Q61D": "A12",
+  "R7ST30V0XHR": "A13",
+  "R7ST31LD00J": "B14",
+  "R7AT310HMKH": "A15",
+  "R7ST405K51K": "A16",
+  "R7AT310P7CM": "A17",
+  "R9YT805BYNL": "A18",
+  "R7AT310P7AV": "A19",
+  "R7ST405MTZP": "A20",
+  "R7SRC1WTKFA": "B21",
+  "R7ST30V31RK": "A22",
+  "R7AT310HPTF": "A23",
+  "R7ST405PJGR": "A24",
+  "R7ST30TVVJE": "C25",
+  "R7AT310NYTR": "A30",
+  "R7ST30TZZ0J": "A31",
+  "R7AT310PLGR": "A32",
+  "R7AT310Q98N": "A33",
+  "R7ST12LK1QJ": "B34",
+  "R7AT310MPTY": "A35",
+  "R7AT310Q6PY": "A36",
+  "R7ST30TZN5V": "A37",
+  "R7AT310PHJB": "A38",
+  "R7AT310NHRE": "A39",
+  "R7ST30V5MHT": "A40",
+  "R7ST405M05E": "A41",
+  "R7AT310P8AB": "A42",
+  "R7AT310QMVP": "A43",
+  "R7ST405K7PP": "A44",
+  "R7AT41BHF2H": "A45",
+  "R7ST30TYCHE": "A46",
+  "R7AT310J26E": "A47",
+  "R7AT310FTBJ": "A48",
+  "R7STB0PS5AA": "A49",
+  "R7AT310NZMK": "A50",
+  "R7ST12LG1QF": "A51",
+  "R7ST31L8DWN": "A52",
+  "R7AT30TZY2R": "A53",
+  "ca2937e9": "A54",
 }
 
 export type Response = {
@@ -73,7 +127,7 @@ export type ApiDevices = {
 
 export type BotFormData = {
   username: string;
-  device: {id: string, battery: string, name: string};
+  device: { id: string, battery: string, name: string };
   password?: string;
   jobs: Jobs,
   config_name?: ConfigNames,
